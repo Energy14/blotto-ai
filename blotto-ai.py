b15d3def292f4635f81d80b256c476c99a49ab4e
Roberts <robsimanis@gmail.com>
2023-04-05 17:38:12 +0300

    base game GUI, switched to Flet

--- blotto-ai.py
+++ blotto-ai.py
@@ -1,18 +1,120 @@
-print("First player input:")
-a1 = int(input())
-a2 = int(input())
-a3 = int(input())
-print("Second player input:")
-b1 = int(input())
-b2 = int(input())
-b3 = int(input())
+import flet as ft
 
-if((b1 + b2 + b3)!=6):
-    print("Invalid input")
+def main(page: ft.Page):
 
-if((a1>b1 and a2>b2) or (a1>b1 and a3>b3) or (a2>b2 and a3>b3)):
-    print("First player wins")
-elif((a1<b1 and a2<b2) or (a1<b1 and a3<b3) or (a2<b2 and a3<b3)):
-    print("Second player wins")
-else:
-    print("Draw")
+    container_color = "#bfbfbf"
+    page.window_width=800
+    page.window_height=600
+    page.window_resizable = False
+    page.window_title = "Blotto War"
+
+    page.add(
+        ft.Column(
+        horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+        controls=[
+            ft.Container(
+                alignment=ft.alignment.center,
+                content=ft.Text("3", size=25, color='black',text_align=ft.TextAlign.CENTER,weight=ft.FontWeight.BOLD),
+                border_radius=90,
+                width=97,
+                height=97,
+                bgcolor=ft.colors.RED_200,
+            ),
+            ft.Row(
+                alignment=ft.MainAxisAlignment.SPACE_AROUND,
+                controls=[
+                ft.Container(
+                    content=
+                        ft.Column(horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                                  alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+                            controls=[
+                            ft.Text("2", size=25, color="#8b0000",weight=ft.FontWeight.BOLD),
+                            ft.Text("Battlefield 1",size=20,color='black'),
+                            ft.Text("0", size=25, color="#023020",weight=ft.FontWeight.BOLD),
+                        ]),   
+                    width=206,
+                    height=285,
+                    border_radius=20,
+                    padding=ft.padding.all(20),
+                    bgcolor=container_color,
+                    ink=True,
+                    on_click=lambda e: print("Clickity 1"),
+                ),
+                ft.Container(
+                    content=
+                        ft.Column(horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                                  alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+                            controls=[
+                            ft.Text("1", size=25, color="#8b0000",weight=ft.FontWeight.BOLD),
+                            ft.Text("Battlefield 2",size=20,color='black'),
+                            ft.Text("1", size=25, color="#023020",weight=ft.FontWeight.BOLD),
+                        ]),   
+                    width=206,
+                    height=285,
+                    border_radius=20,
+                    padding=ft.padding.all(20),
+                    bgcolor=container_color,
+                    ink=True,
+                    on_click=lambda e: print("Clickity 2"),
+                ),
+                ft.Container(
+                    content=
+                        ft.Column(horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                                  alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+                            controls=[
+                            ft.Text("0", size=25, color="#8b0000",weight=ft.FontWeight.BOLD),
+                            ft.Text("Battlefield 3",size=20,color='black'),
+                            ft.Text("0", size=25, color="#023020",weight=ft.FontWeight.BOLD),
+                        ]),   
+                    width=206,
+                    height=285,
+                    border_radius=20,
+                    padding=ft.padding.all(20),
+                    bgcolor=container_color,
+                    ink=True,
+                    on_click=lambda e: print("Clickity 3"),
+                )
+            ]),
+            ft.Row(
+                alignment=ft.MainAxisAlignment.SPACE_AROUND,
+                controls=[
+                ft.ElevatedButton(
+                    content=ft.Text("Reset", size=20, color='black',text_align=ft.TextAlign.CENTER,weight=ft.FontWeight.BOLD),
+                    style=ft.ButtonStyle(
+                        color={
+                            ft.MaterialState.DEFAULT: ft.colors.BLACK,
+                        },
+                        bgcolor={
+                            ft.MaterialState.DEFAULT: ft.colors.BLUE_200,
+                            ft.MaterialState.HOVERED: ft.colors.RED_400,
+                        },
+                        padding=ft.padding.only(43,20,43,20),
+                    )
+                ),
+                ft.Container(
+                    alignment=ft.alignment.center,
+                    content=ft.Text("5", size=25, color='black',text_align=ft.TextAlign.CENTER,weight=ft.FontWeight.BOLD),
+                    border_radius=90,
+                    width=97,
+                    height=97,
+                    bgcolor=ft.colors.GREEN_200,
+                ),
+                ft.ElevatedButton(
+                    content=ft.Text("End turn", size=20, color='black',text_align=ft.TextAlign.CENTER,weight=ft.FontWeight.BOLD),
+                    style=ft.ButtonStyle(
+                        color={
+                            ft.MaterialState.DEFAULT: ft.colors.BLACK,
+                        },
+                        bgcolor={
+                            ft.MaterialState.DEFAULT: ft.colors.BLUE_200,
+                            ft.MaterialState.HOVERED: ft.colors.GREEN_500,
+                        },
+                        padding=ft.padding.only(30,20,30,20),
+                    ),
+                    on_click=lambda e: print("Clickity End turn"),
+                )
+            ])
+        ])
+    )
+
+ft.app(target=main)
